6381f6e4240b2a7f80e4a51d29208f654b7e9888
woodstyle <denver00acebedo@gmail.com>
2015-02-21 23:55:41 -0800

    1st(main)

---
+++ SaplePython/dbMysql.py
@@ -0,0 +1,28 @@
+__author__ = 'Mary Grace'
+import MySQLdb
+
+
+#Setting up Connection
+db = MySQLdb.connect("127.0.0.1","root","passes12345","try_student")
+cursor = db.cursor()
+
+studId=raw_input("Enter Student Id: ")
+name = raw_input("Enter Student Naame: ")
+
+
+sql = "Insert into student(studentID, studentName)\
+      Values ('%s', '%s')" % \
+      (studId, name)
+
+try:
+    cursor.execute(sql)
+    db.commit()
+    print "Data was Added"
+except:
+    db.rollback()
+    print "There is a problem encountered"
+
+
+
+db.close()
+
